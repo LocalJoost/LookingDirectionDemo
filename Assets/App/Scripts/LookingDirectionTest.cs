42772a094af89316dda40b14e531bff5fff6deaf
Joost van Schaik <joost.van.schaik@wortell.nl>
2017-06-27 13:25:35 +0200

    Initial code checkin

---
+++ Assets/App/Scripts/LookingDirectionTest.cs
@@ -0,0 +1,16 @@
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkitExtensions.Utilities;
+using UnityEngine;
+
+public class LookingDirectionTest : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Debug.Log(LookingDirectionHelpers.GetPostionInLookingDirection(2.0f,GazeManager.Instance.Stabilizer));
+    }
+}
